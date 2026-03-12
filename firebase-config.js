e03ca0cfd296cd2729cbfec47305a8ef288f565b
liuchenghsin <92716087+liuchenghsin@users.noreply.github.com>
2026-03-12 21:40:46 +0800

    Refactor Firebase configuration to use module imports

--- firebase-config.js
+++ firebase-config.js
@@ -1,31 +1,22 @@
-{\rtf1\ansi\ansicpg950\cocoartf2868
-\cocoatextscaling0\cocoaplatform0{\fonttbl\f0\fswiss\fcharset0 Helvetica;}
-{\colortbl;\red255\green255\blue255;}
-{\*\expandedcolortbl;;}
-\paperw11900\paperh16840\margl1440\margr1440\vieww11520\viewh8400\viewkind0
-\pard\tx720\tx1440\tx2160\tx2880\tx3600\tx4320\tx5040\tx5760\tx6480\tx7200\tx7920\tx8640\pardirnatural\partightenfactor0
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
 
-\f0\fs24 \cf0 <script type="module">\
-  // Import the functions you need from the SDKs you need\
-  import \{ initializeApp \} from "https://www.gstatic.com/firebasejs/12.10.0/firebase-app.js";\
-  import \{ getAnalytics \} from "https://www.gstatic.com/firebasejs/12.10.0/firebase-analytics.js";\
-  // TODO: Add SDKs for Firebase products that you want to use\
-  // https://firebase.google.com/docs/web/setup#available-libraries\
-\
-  // Your web app's Firebase configuration\
-  // For Firebase JS SDK v7.20.0 and later, measurementId is optional\
-  const firebaseConfig = \{\
-    apiKey: "AIzaSyDNkmLQbmaUMgN3ASAg7gi-U-zn9AMDvAM",\
-    authDomain: "passworddefender-73aab.firebaseapp.com",\
-    databaseURL: "https://passworddefender-73aab-default-rtdb.firebaseio.com",\
-    projectId: "passworddefender-73aab",\
-    storageBucket: "passworddefender-73aab.firebasestorage.app",\
-    messagingSenderId: "270944179994",\
-    appId: "1:270944179994:web:0c36cf807ace1b6fe7bea3",\
-    measurementId: "G-QGPW9LVD4V"\
-  \};\
-\
-  // Initialize Firebase\
-  const app = initializeApp(firebaseConfig);\
-  const analytics = getAnalytics(app);\
-</script>}
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyDNkmLQbmaUMgN3ASAg7gi-U-zn9AMDvAM",
+  authDomain: "passworddefender-73aab.firebaseapp.com",
+  databaseURL: "https://passworddefender-73aab-default-rtdb.firebaseio.com",
+  projectId: "passworddefender-73aab",
+  storageBucket: "passworddefender-73aab.firebasestorage.app",
+  messagingSenderId: "270944179994",
+  appId: "1:270944179994:web:0c36cf807ace1b6fe7bea3",
+  measurementId: "G-QGPW9LVD4V"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
